383b96eb637e971e557513c3d1f64315b8e5af47
GPGamer98 <89650503+GPGamer98@users.noreply.github.com>
2024-05-05 18:05:57 +0200

    fix

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = global.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "development") {
+if (process.env.NODE_ENV === "development") {
     global.prisma = prisma;
 }
 
