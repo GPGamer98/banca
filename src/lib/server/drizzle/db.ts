d2d5e4e66a252e9a529d1eeeb83e771dff527f24
GPGamer98 <89650503+GPGamer98@users.noreply.github.com>
2024-05-09 17:43:35 +0200

    hahahahahahahahaahaaaaaaaaaaaaa

--- src/lib/server/drizzle/db.ts
+++ src/lib/server/drizzle/db.ts
@@ -2,5 +2,5 @@ import { drizzle } from "drizzle-orm/better-sqlite3";
 import * as schema from "./schema";
 import Database from "better-sqlite3";
 
-const client = new Database("./db/sqlite.db");
+const client = new Database('src/lib/server/drizzle/db/sqlite.db');
 export const db = drizzle(client, { schema, logger: true });
